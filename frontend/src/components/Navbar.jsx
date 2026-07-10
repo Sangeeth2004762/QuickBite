3e008521a3bbbb07b33848111c90469c8e5dc4bb
Sangeeth2004762 <2403717620521044@cit.edu.in>
2026-07-10 21:32:23 +0530

    Delete frontend directory

--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="tm-navbar">
-      <div className="tm-nav-logo">🍜 QuickBite</div>
-      <span className="tm-nav-title">Task Manager</span>
-      <a href="#" className="tm-nav-link">Order Dashboard</a>
-    </nav>
-  );
-}
